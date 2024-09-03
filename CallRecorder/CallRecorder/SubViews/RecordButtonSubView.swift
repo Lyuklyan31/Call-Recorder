c064c371c925a19ccd3263c936ef9c77c567b966
Andrii Boichuk <a.boichuk@extern.zhortech.com>
2024-09-04 00:29:10 +0300

    Second Button Microphone.

--- CallRecorder/CallRecorder/SubViews/RecordButtonSubView.swift
+++ CallRecorder/CallRecorder/SubViews/RecordButtonSubView.swift
@@ -2,14 +2,35 @@
 //  RecordButtonSubView.swift
 //  CallRecorder
 //
-//  Created by Andrii Boichuk on 03.09.2024.
+//  Created by Andrii Boichuk on 04.09.2024.
 //
 
 import SwiftUI
 
 struct RecordButtonSubView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+            
+        } label: {
+            ZStack {
+                Circle()
+                    .frame(width: 240)
+                    .foregroundColor(.customPink.opacity(0.1))
+                
+                Circle()
+                    .frame(width: 192)
+                    .foregroundColor(.customPink.opacity(0.1))
+                
+                Circle()
+                    .frame(width: 142)
+                    .foregroundColor(.white)
+                
+                Image(.microphone)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 80, height: 80)
+            }
+        }
     }
 }
 
