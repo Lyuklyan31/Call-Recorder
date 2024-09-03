d321d43697d6b77bc886be2a3702298639f97e34
Andrii Boichuk <a.boichuk@extern.zhortech.com>
2024-09-03 23:22:17 +0300

    created the project

---
+++ CallRecorder/CallRecorder/CallRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CallRecorderApp.swift
+//  CallRecorder
+//
+//  Created by Andrii Boichuk on 03.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct CallRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
