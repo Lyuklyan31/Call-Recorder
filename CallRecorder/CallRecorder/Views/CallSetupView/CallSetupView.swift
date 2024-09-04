b98d8851fa1aaecefa90a5e30db18dc825594265
Andrii Boichuk <a.boichuk@extern.zhortech.com>
2024-09-04 12:36:49 +0300

    Setup Navigation bar + text in setting.

--- CallRecorder/CallRecorder/Views/CallSetupView/CallSetupView.swift
+++ CallRecorder/CallRecorder/Views/CallSetupView/CallSetupView.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct CallSetupView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            CallSetupNavigationBar()
+            
+            Text("Enter your phone number")
+                .font(.system(size: 19, weight: .medium ))
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+            
+            Spacer()
+                .navigationBarBackButtonHidden()
+        }
     }
 }
 
