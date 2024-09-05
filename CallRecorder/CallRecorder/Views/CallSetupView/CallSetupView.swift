15ab53761c90fb954c69559b87ba0068732fd831
Andrii Boichuk <a.boichuk@extern.zhortech.com>
2024-09-05 16:13:17 +0300

    Call setup setting

--- CallRecorder/CallRecorder/Views/CallSetupView/CallSetupView.swift
+++ CallRecorder/CallRecorder/Views/CallSetupView/CallSetupView.swift
@@ -8,17 +8,45 @@
 import SwiftUI
 
 struct CallSetupView: View {
+    @State private var selectedCountry: String? = "Ukraine"
+    @State private var selectedFlag: String? = "🇺🇦"
+    @State private var selectedDialCode: String? = "+380"
+    @State private var phoneNumber: Int?
     var body: some View {
-        VStack {
-            CallSetupNavigationBar()
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(colors: [.backraundWhite, .backraundPink]),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .edgesIgnoringSafeArea(.all)
             
-            Text("Enter your phone number")
-                .font(.system(size: 19, weight: .medium ))
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding()
-            
-            Spacer()
-                .navigationBarBackButtonHidden()
+            VStack {
+                CallSetupNavigationBar()
+                
+                Text("Enter your phone number")
+                    .foregroundColor(.primaryExtraDark)
+                    .font(.system(size: 19, weight: .medium ))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+                
+                ChooseCountryDialCode(selectionCountry: $selectedCountry, selectionFlag: $selectedFlag, selectionDialCode: $selectedDialCode)
+                
+                PhoneNumberTextField(selectionDialCode: $selectedDialCode, phoneNumber: $phoneNumber)
+                    .padding(.horizontal)
+                
+                Text("Ensure that you can receive SMS on this number so we can send you a code.")
+                    .font(.subheadline)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.primaryExtraDark.opacity(0.5))
+                    .padding()
+                    .padding(.horizontal, 40)
+                    
+                ContinueButton(amountNumbers: $phoneNumber)
+                    .padding(.horizontal)
+                Spacer()
+                    .navigationBarBackButtonHidden()
+            }
         }
     }
 }
