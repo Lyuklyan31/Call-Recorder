54caec5b059aa3dc2ea70b0094b83a893b6cf47f
Andrii Boichuk <a.boichuk@extern.zhortech.com>
2024-09-08 03:49:20 +0300

    created SettingView

--- CallRecorder/CallRecorder/Views/Seting/SettingView.swift
+++ CallRecorder/CallRecorder/Views/Seting/SettingView.swift
@@ -9,7 +9,31 @@ import SwiftUI
 
 struct SettingView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(colors: [.backraundWhite, .backraundPink]),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                NavigationBarSubView(title: "Settings")
+                ButtonSetting(image: "shareApp", text: "Share App", destination: Waves())
+                
+                ButtonSetting(image: "restorePurchases", text: "Restore Purchases", destination: Waves())
+                
+                ButtonSetting(image: "rateUs", text: "Rate Us", destination: Waves())
+                
+                ButtonSetting(image: "contactUs", text: "Contact Us", destination: Waves())
+                
+                ButtonSetting(image: "termsOfUse", text: "Terms of Use", destination: Waves())
+                
+                ButtonSetting(image: "privacyPolicy", text: "Privacy Policy", destination: Waves())
+                Spacer()
+                    .navigationBarBackButtonHidden()
+            }
+        }
     }
 }
 
