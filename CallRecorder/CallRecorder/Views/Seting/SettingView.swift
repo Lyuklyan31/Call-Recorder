f7f02a0a38ed79fcc8b86d1ac468bae65662798f
Andrii Boichuk <a.boichuk@extern.zhortech.com>
2024-09-08 03:45:07 +0300

    Added images

---
+++ CallRecorder/CallRecorder/Views/Seting/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  CallRecorder
+//
+//  Created by Andrii Boichuk on 07.09.2024.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
---
+++ CallRecorder/CallRecorder/Views/Seting/SubViews/ButtonSetting.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonSetting.swift
+//  CallRecorder
+//
+//  Created by Andrii Boichuk on 07.09.2024.
+//
+
+import SwiftUI
+
+struct ButtonSetting: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonSetting()
+}
