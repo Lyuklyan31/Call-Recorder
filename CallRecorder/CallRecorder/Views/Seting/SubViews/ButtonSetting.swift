ab1cb4c023dab3c2357f953f0c6a339d1f00d239
Andrii Boichuk <a.boichuk@extern.zhortech.com>
2024-09-08 03:46:08 +0300

    Button for view setting

--- CallRecorder/CallRecorder/Views/Seting/SubViews/ButtonSetting.swift
+++ CallRecorder/CallRecorder/Views/Seting/SubViews/ButtonSetting.swift
@@ -7,12 +7,35 @@
 
 import SwiftUI
 
-struct ButtonSetting: View {
+struct ButtonSetting<Destination: View>: View {
+    var image: String
+    var text: String
+    var destination: Destination
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationLink(destination: destination) {
+            ZStack {
+                RoundedRectangle(cornerRadius: 24)
+                    .foregroundColor(.white)
+                    .frame(height: 64)
+                
+                HStack {
+                    Image(image)
+                        .padding(.horizontal)
+                    Text(text)
+                        .font(.system(size: 19))
+                        .bold()
+                        .foregroundColor(.black)
+                    Spacer()
+                    Image(.arrowRight)
+                        .padding(.horizontal)
+                }
+            }
+            .padding(.horizontal)
+        }
     }
 }
 
 #Preview {
-    ButtonSetting()
+    ButtonSetting(image: "rateUs", text: "Text", destination: Text("Destination View"))
 }
