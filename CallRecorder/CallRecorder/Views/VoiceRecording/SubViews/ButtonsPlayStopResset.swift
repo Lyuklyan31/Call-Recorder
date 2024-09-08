3300442dea98feb5d43b61364d9ea214c6e979d9
Andrii Boichuk <a.boichuk@extern.zhortech.com>
2024-09-08 03:46:40 +0300

    setting button play stop. reset

--- CallRecorder/CallRecorder/Views/VoiceRecording/SubViews/ButtonsPlayStopResset.swift
+++ CallRecorder/CallRecorder/Views/VoiceRecording/SubViews/ButtonsPlayStopResset.swift
@@ -8,13 +8,20 @@
 import SwiftUI
 
 struct ButtonsPlayStopResset: View {
-    @ObservedObject var viewModel: VoiceRecordingViewModel
+    @ObservedObject var viewModel: TimerViewModel
     
+    @EnvironmentObject var wavesViewModel: WavesViewModel
+    @EnvironmentObject var audioRecorder: AudioRecorder
+    
+    @State private var showingAlert = false
     var body: some View {
         HStack {
             Spacer()
             Button {
+//                audioRecorder.startRecording()
+                viewModel.timerIsRunning ? wavesViewModel.stopMonitoring() : wavesViewModel.startMonitoring()
                 viewModel.timerIsRunning ? viewModel.stopTimer() : viewModel.startTimer()
+                
             } label: {
                 RoundedRectangle(cornerRadius: 24)
                     .frame(width: 64, height: 64)
@@ -25,9 +32,11 @@ struct ButtonsPlayStopResset: View {
             }
            
             Spacer()
-            
+          
             Button {
                 viewModel.resetTimer()
+//                audioRecorder.resetRecording()
+                showingAlert = true
             } label: {
                 RoundedRectangle(cornerRadius: 24)
                     .frame(width: 64, height: 64)
@@ -36,11 +45,27 @@ struct ButtonsPlayStopResset: View {
                         Image(.resset)
                     }
             }
+            .disabled(viewModel.timerIsRunning == false)
             Spacer()
         }
+        .alert(isPresented: $showingAlert) {
+            Alert(
+                title: Text("Cancel Recording?"),
+                message: Text("Cancelling the recording will discard any unsaved data. Continue?"),
+                primaryButton: .destructive(Text("Cancel")) {
+                    wavesViewModel.stopMonitoring()
+                },
+                secondaryButton: .cancel(Text("Back")) {
+                   
+                }
+            )
+        }
+
     }
 }
 
 #Preview {
-    ButtonsPlayStopResset(viewModel: VoiceRecordingViewModel())
+    ButtonsPlayStopResset(viewModel: TimerViewModel())
+        .environmentObject(AudioRecorder())
+        .environmentObject(WavesViewModel())
 }
