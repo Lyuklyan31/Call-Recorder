cb39e05796cab9faaad5d883c2a646e077c7029a
Andrii Boichuk <a.boichuk@extern.zhortech.com>
2024-09-14 18:15:39 +0300

    add buttons tag and note

---
+++ CallRecorder/CallRecorder/MainSubViews/VoiceRecords/VoiceRecords/PlayerSheet/ButtonsTagAndNote/ButtonsTagAndNote.swift
@@ -0,0 +1,50 @@
+//
+//  ButtonTag.swift
+//  CallRecorder
+//
+//  Created by Andrii Boichuk on 14.09.2024.
+//
+
+import SwiftUI
+
+struct ButtonsTagAndNote: View {
+    @EnvironmentObject var audioPlayer: AudioPlayer
+    var audioURL: URL
+    
+    var body: some View {
+        HStack {
+            Spacer()
+            
+            Button {
+                
+            } label: {
+                VStack {
+                    Image(.tag)
+                    Text("Tag")
+                        .foregroundColor(.primaryExtraDark.opacity(0.5))
+                        .font(.system(size: 15, weight: .regular))
+                }
+            }
+            
+            Spacer()
+            
+            Button {
+                
+            } label: {
+                VStack {
+                    Image(.note)
+                    Text("Note")
+                        .foregroundColor(.primaryExtraDark.opacity(0.5))
+                        .font(.system(size: 15, weight: .regular))
+                }
+            }
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    ButtonsTagAndNote(audioURL: URL(string: "https://www.example.com/audiofile.m4a")!)
+        .environmentObject(AudioRecorder())
+}
